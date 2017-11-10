39193d7c27fbf11ed06d316da57b2deaa2675a7c
Reetii <reetigarg12@gmail.com>
2017-11-10 05:45:39 -0800

    Made the changes in modal and added the link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  names=[];
   name = '';
+  pushName= function(){
+    if(this.name != ''){
+      this.names.push(this.name);
+      this.name='';
+    }
+  }
+  gotoLink=function(){
+    window.open('https://angular.io/docs');
+  }
 }
